b09b7bbe7c6d35f42b54293d276efc8686f95941
KoiCode7 <121136830+KoiCode7@users.noreply.github.com>
2023-04-09 13:06:50 -0400

    Upload my project

---
+++ main.js
@@ -0,0 +1,101 @@
+"use strict";
+
+{
+  // function render(quiz) {
+  //   const main = document.querySelector("main");
+
+  //   const section = document.createElement("section");
+  //   const h2 = document.createElement("h2");
+  //   h2.textContent = quiz[0];
+
+  //   const ul = document.createElement("ul");
+
+  //   const li0 = document.createElement("li");
+  //   li0.textContent = quiz[1];
+  //   li0.addEventListener("click", () => {
+  //     if (quiz[4] === 0) {
+  //       li0.classList.add("correct");
+  //     } else {
+  //       li0.classList.add("wrong");
+  //     }
+  //   });
+  //   const li1 = document.createElement("li");
+  //   li1.textContent = quiz[2];
+  //   li1.addEventListener("click", () => {
+  //     if (quiz[4] === 1) {
+  //       li1.classList.add("correct");
+  //     } else {
+  //       li1.classList.add("wrong");
+  //     }
+  //   });
+  //   const li2 = document.createElement("li");
+  //   li2.textContent = quiz[3];
+  //   li2.addEventListener("click", () => {
+  //     if (quiz[4] === 2) {
+  //       li2.classList.add("correct");
+  //     } else {
+  //       li2.classList.add("wrong");
+  //     }
+  //   });
+
+  //   ul.appendChild(li0);
+  //   ul.appendChild(li1);
+  //   ul.appendChild(li2);
+
+  //   section.appendChild(h2);
+  //   section.appendChild(ul);
+  //   main.appendChild(section);
+  // }
+
+  // const quizzes = [
+  //   ["Q1: What's your answer?", "Choice: A", "Choice: B", "Choice: C", 0],
+  //   ["Q2: What's your answer?", "Choice: A", "Choice: B", "Choice: C", 1],
+  //   ["Q3: What's your answer?", "Choice: A", "Choice: B", "Choice: C", 2],
+  // ];
+
+  // quizzes.forEach((quiz) => {
+  //   render(quiz);
+  // });
+
+
+
+  // After refactoring
+  function render(quiz) {
+    const main = document.querySelector("main");
+
+    const section = document.createElement("section");
+    const h2 = document.createElement("h2");
+    h2.textContent = quiz[0];
+
+    const ul = document.createElement("ul");
+
+    const li = {};
+
+    for (let i = 0; i < quiz.length - 2; i++) {
+      li[i] = document.createElement("li");
+      li[i].textContent = quiz[i + 1];
+      li[i].addEventListener("click", () => {
+        if (quiz[4] === i) {
+          li[i].classList.add("correct");
+        } else {
+          li[i].classList.add("wrong");
+        }
+      });
+      ul.appendChild(li[i]);
+    }
+
+    section.appendChild(h2);
+    section.appendChild(ul);
+    main.appendChild(section);
+  }
+
+  const quizzes = [
+    ["Q1: What's your answer?", "Choice: A", "Choice: B", "Choice: C", 0],
+    ["Q2: What's your answer?", "Choice: A", "Choice: B", "Choice: C", 1],
+    ["Q3: What's your answer?", "Choice: A", "Choice: B", "Choice: C", 2],
+  ];
+
+  quizzes.forEach((quiz) => {
+    render(quiz);
+  });
+}
